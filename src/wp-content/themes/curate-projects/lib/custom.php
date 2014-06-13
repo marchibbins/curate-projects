2adc497a03f76fb223bef2c3738be60f8314b541
Marc Hibbins <marchibbins@gmail.com>
2014-06-13 21:50:14 +0100

    Add custom array functions

--- src/wp-content/themes/curate-projects/lib/custom.php
+++ src/wp-content/themes/curate-projects/lib/custom.php
@@ -2,3 +2,13 @@
 /**
  * Custom functions
  */
+
+function first(&$array, $key) {
+    reset($array);
+    return $key === key($array);
+}
+
+function last(&$array, $key) {
+    end($array);
+    return $key === key($array);
+}
