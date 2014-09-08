00f08f164a2199065361a2504803269543cf33e7
Marc Hibbins <marchibbins@gmail.com>
2014-09-08 20:04:37 +0100

    Styles for initial footer

--- src/wp-content/themes/curate-projects/lib/scripts.php
+++ src/wp-content/themes/curate-projects/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '380b5e491a744f9ae23d4e3ffd8ba20d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5a163e2dd9104ac658091e420aa6fe88');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- src/wp-content/themes/curate-projects/templates/footer.php
+++ src/wp-content/themes/curate-projects/templates/footer.php
@@ -1,7 +1,18 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>
 
-<?php wp_footer(); ?>
+<footer class="site-footer" role="contentinfo">
+    <div class="container">
+        <div class="col-sm-6">
+            <?php dynamic_sidebar('sidebar-footer'); ?>
+        </div>
+        <div class="col-sm-6">
+            <nav class="menu">
+                <ul class="list-unstyled pull-right">
+                    <li><a class="pull-right" href="#" title="Subscribe to us on Facebook">Facebook</a></li>
+                    <li><a class="pull-right" href="#" title="Follow us on Twitter">Twitter</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <?php wp_footer(); ?>
+    </div>
+</footer>
