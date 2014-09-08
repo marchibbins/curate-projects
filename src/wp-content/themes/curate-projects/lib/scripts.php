a961e6830a97e72b575b2823013e434489990f0d
Marc Hibbins <marchibbins@gmail.com>
2014-09-08 21:38:33 +0100

    Move gallery less file

--- src/wp-content/themes/curate-projects/lib/scripts.php
+++ src/wp-content/themes/curate-projects/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('owl-carousel', get_template_directory_uri() . '/assets/js/vendor/owl-carousel/assets/owl.carousel.css', false);
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '15f2412e3ef01958717c781dbe3ed1ef');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5fe1ae34c035311563af9bd3a5b5fcfa');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
