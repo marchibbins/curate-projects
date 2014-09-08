d9f75e1a7636679ad643348b193931a04e5714fb
Marc Hibbins <marchibbins@gmail.com>
2014-09-08 18:54:21 +0100

    Add temporary page design

--- src/wp-content/themes/curate-projects/page.php
+++ src/wp-content/themes/curate-projects/page.php
@@ -1,4 +1,15 @@
 
+<style type="text/css" media="screen">
+    .gallery-header {
+        background: url(http://lorempixel.com/1000/200/abstract/1/);
+        background-size: cover;
+        height: 200px;
+        margin: 0 -15px;
+    }
+</style>
+
+<div class="gallery-header"></div>
+
 <div class="container">
     <?php get_template_part('templates/page', 'header'); ?>
     <?php get_template_part('templates/content', 'page'); ?>
