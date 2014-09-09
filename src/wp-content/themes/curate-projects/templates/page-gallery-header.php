d45a55732f6d7a4dff6aeb031db48a4abca269f0
Marc Hibbins <marchibbins@gmail.com>
2014-09-09 10:46:15 +0100

    Clean up gallery header

--- src/wp-content/themes/curate-projects/templates/page-gallery-header.php
+++ src/wp-content/themes/curate-projects/templates/page-gallery-header.php
@@ -1,27 +1,28 @@
 <?php
-$featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
-$media = get_attached_media( 'image' );
+    $featured = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+    $media = get_attached_media( 'image' );
 ?>
 
-<? if(!empty($featured)):?>
+<? if (!empty($featured)): ?>
 
-<div class="gallery-header" style="background-image:url('<?= $featured[0] ?>')"></div>
+    <div class="gallery-header" style="background-image:url('<?= $featured[0] ?>')"></div>
 
-<? elseif($media):?>
+<? elseif ($media): ?>
 
-<div class="owl-carousel-container">
-  <ul class="owl-carousel">
-  <? foreach ( $media as $image ):
-  $src = wp_get_attachment_image_src($image->ID, 'medium'); 
-  $caption = get_post_field('post_excerpt', $image->ID); 
-?>
-    <li class="item" >
-      <img src="<?=$src[0] ?>" />
-      <? if(!empty($caption)):?>
-      <span class="caption"><?= $caption?></span>
-      <? endif ?>
-    </li>
-  <? endforeach ?>
+    <div class="owl-carousel-container">
+        <ul class="owl-carousel">
+            <? foreach ( $media as $image ):
+                $src = wp_get_attachment_image_src($image->ID, 'medium');
+                $caption = get_post_field('post_excerpt', $image->ID);
+            ?>
+                <li class="item" >
+                    <img src="<?=$src[0] ?>">
+                    <? if (!empty($caption)): ?>
+                        <span class="caption"><?= $caption ?></span>
+                    <? endif ?>
+                </li>
+            <? endforeach ?>
+        </ul>
+    </div>
+
 <? endif ?>
-  </ul>
-</div>
