5c570037b8a2a77358266376530d65584d66ef0d
Marc Hibbins <marchibbins@gmail.com>
2014-09-22 20:03:19 +0100

    Move child page menu into custom

--- src/wp-content/themes/curate-projects/lib/custom.php
+++ src/wp-content/themes/curate-projects/lib/custom.php
@@ -14,4 +14,17 @@ function last(&$array, $key) {
     return $key === key($array);
 }
 
+function get_child_pages($wpdb, $id) {
+    $query = "
+        SELECT $wpdb->posts.*
+        FROM $wpdb->posts
+        WHERE $wpdb->posts.post_parent = " . $id . "
+        AND $wpdb->posts.post_status = 'publish'
+        AND $wpdb->posts.post_type = 'page'
+        AND $wpdb->posts.post_date < '" . current_time('mysql') . "'
+        ORDER BY $wpdb->posts.menu_order ASC
+    ";
+    return $wpdb->get_results($query, OBJECT);
+}
+
 add_filter('show_admin_bar', '__return_false');
--- src/wp-content/themes/curate-projects/templates/page-header.php
+++ src/wp-content/themes/curate-projects/templates/page-header.php
@@ -2,24 +2,16 @@
     <h1><?php echo roots_title(); ?></h1>
 
     <?php
-        $query = "
-            SELECT $wpdb->posts.*
-            FROM $wpdb->posts
-            WHERE $wpdb->posts.post_parent = ".get_the_ID()."
-            AND $wpdb->posts.post_status = 'publish'
-            AND $wpdb->posts.post_type = 'page'
-            AND $wpdb->posts.post_date < '".current_time('mysql')."'
-            ORDER BY $wpdb->posts.menu_order ASC
-        ";
-        $child_pages = $wpdb->get_results($query, OBJECT);
+        $id = $post->post_parent ? $post->post_parent : get_the_ID();
+        $child_pages = get_child_pages($wpdb, $id);
     ?>
 
     <?php if ($child_pages): ?>
         <nav>
             <ul class="nav nav-pills child-page-nav">
                 <?php foreach ($child_pages as $key => $child_page): ?>
-                    <li class="<?= first($child_pages, $key) ? 'active' : ''; ?>">
-                        <a href="<?= get_permalink() . $child_page->post_name ?>"><?= $child_page->post_title; ?></a>
+                    <li class="<?= get_the_ID() == $child_page->ID ? 'active' : ''; ?>">
+                        <a href="<?= get_permalink($child_page->post_parent) . $child_page->post_name ?>"><?= $child_page->post_title; ?></a>
                     </li>
                 <?php endforeach; ?>
             </ul>
