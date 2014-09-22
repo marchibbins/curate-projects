e8a32560ade0d315322fb018dd3f25b7d4b213a5
garethfoote <gareth.foote@gmail.com>
2014-09-22 21:32:20 +0100

    Removed redundant styling on figure caption

--- src/wp-content/themes/curate-projects/lib/scripts.php
+++ src/wp-content/themes/curate-projects/lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function roots_scripts() {
   wp_enqueue_style('owl-carousel', get_template_directory_uri() . '/assets/js/vendor/owl-carousel/assets/owl.carousel.css', false);
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2ba718d7756b533ef9fa264740cff48f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '19804d18722fee432bf0dae181f5d499');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
