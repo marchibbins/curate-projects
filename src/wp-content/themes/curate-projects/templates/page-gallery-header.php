9db13ebc77eeb1edb0fb4aaa6699535ce5e46e4c
garethfoote <gareth.foote@gmail.com>
2014-09-22 20:49:53 +0100

    Added lightbox, upped image quality and added padding to carousel£

--- src/wp-content/themes/curate-projects/templates/page-gallery-header.php
+++ src/wp-content/themes/curate-projects/templates/page-gallery-header.php
@@ -12,14 +12,13 @@
     <div class="owl-carousel-container">
         <ul class="owl-carousel">
             <? foreach ( $media as $image ):
-                $src = wp_get_attachment_image_src($image->ID, 'medium');
+                $src = wp_get_attachment_image_src($image->ID, 'large');
                 $caption = get_post_field('post_excerpt', $image->ID);
             ?>
-                <li class="item" >
-                    <img src="<?=$src[0] ?>">
-                    <? if (!empty($caption)): ?>
-                        <span class="caption"><?= $caption ?></span>
-                    <? endif ?>
+                <li class="item">
+                  <a href="<?=$src[0]?>" title="<?= $caption?>" rel="lightbox-0">
+                    <img src="<?=$src[0] ?>" alt="<?= $caption?>">
+                  </a>
                 </li>
             <? endforeach ?>
         </ul>
