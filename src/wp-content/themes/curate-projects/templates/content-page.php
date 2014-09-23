67f7e285f1edc71feeebf3100a5cca9ffdf85ab1
garethfoote <gareth.foote@gmail.com>
2014-09-23 09:54:15 +0100

    Made child pages into tabs on parent page

--- src/wp-content/themes/curate-projects/templates/content-page.php
+++ src/wp-content/themes/curate-projects/templates/content-page.php
@@ -1,4 +1,24 @@
 <?php while (have_posts()) : the_post(); ?>
+
+<div class="tab-content">
+<?php 
+    $id = get_the_ID();
+    $args = array(
+      'post_type'      => 'page',
+      'posts_per_page' => -1,
+      'post_parent'    => $id,
+      'order'          => 'ASC',
+      'orderby'        => 'menu_order'
+    );
+    $children = new WP_Query( $args );
+    while($children->have_posts()): $children->the_post();
+?>
+  <div class="tab-pane <?= $children->current_post === 0 ? 'active' : ''; ?>" id="<?= get_the_ID() ?>"><? the_content() ?></div>
+<?php endwhile; // end child pages 
+wp_reset_postdata();
+?>
+</div><!--end child page tabs -->
+
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
--- src/wp-content/themes/curate-projects/templates/page-header.php
+++ src/wp-content/themes/curate-projects/templates/page-header.php
@@ -2,18 +2,25 @@
     <h1><?php echo roots_title(); ?></h1>
 
     <?php
-        $id = $post->post_parent ? $post->post_parent : get_the_ID();
-        $child_pages = get_child_pages($wpdb, $id);
+        $id = get_the_ID();
+        $args = array(
+          'post_type'      => 'page',
+          'posts_per_page' => -1,
+          'post_parent'    => $id,
+          'order'          => 'ASC',
+          'orderby'        => 'menu_order'
+        );
+        $children = new WP_Query( $args );
     ?>
 
-    <?php if ($child_pages): ?>
+    <?php if ($children->have_posts()): ?>
         <nav>
             <ul class="nav child-page-nav">
-                <?php foreach ($child_pages as $key => $child_page): ?>
-                    <li class="<?= get_the_ID() == $child_page->ID ? 'active' : ''; ?>">
-                        <a href="<?= get_permalink($child_page->post_parent) . $child_page->post_name ?>"><?= $child_page->post_title; ?></a>
+                <?php while ($children->have_posts()): $children->the_post(); ?>
+                    <li class="<?= $children->current_post === 0 ? 'active' : ''; ?>">
+                        <a role="tab" data-toggle="tab" href="#<?= get_the_ID() ?>"><?= get_the_title(); ?></a>
                     </li>
-                <?php endforeach; ?>
+                <?php endwhile; ?>
             </ul>
         </nav>
     <?php endif; ?>
