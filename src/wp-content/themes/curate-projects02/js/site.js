5a498667f533773cbc30e43dc867d14c90fe280f
Marc Hibbins <marchibbins@gmail.com>
2015-05-12 11:45:03 +0100

    Mobile nav toggle

--- src/wp-content/themes/curate-projects02/js/site.js
+++ src/wp-content/themes/curate-projects02/js/site.js
@@ -3,8 +3,7 @@ jQuery( document ).ready( function( $ ) {
   $('.js-toggle-wrapper').on('click', function(e){
 
     e.preventDefault();
-    $('.Wrapper-left').toggleClass('is-open');
-    $(this).toggleClass('is-open');
+    $('.Banner .Nav--primary').toggleClass('is-open');
   });
 
 
